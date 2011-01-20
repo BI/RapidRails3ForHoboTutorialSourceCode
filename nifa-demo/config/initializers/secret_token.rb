cf8097f3a750e5d6857696b38778fce4d3e6d75f
unknown <vashtakala@.(none)>
2011-01-20 11:40:27 +0530

    Loaded the nifa-demo application which is used in Tutorial 21

---
+++ nifa-demo/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+NifaDemo::Application.config.secret_token = '1911323eb58c86c508e046f8c4f2526a6307ff499ac09de7daf320dfc29a0ac4930a3ca77efc9ea22544388ade530740331655fa531f2b43c0f2694965ca1531'
